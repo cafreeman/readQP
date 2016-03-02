c7ee270bd065f7be28561a3a2f75abd8505f0a50
Chris Freeman <cfreeman.code@gmail.com>
2016-03-02 16:04:25 -0600

    Add test data and a basic test for `readQP`

---
+++ tests/testthat/test_readQP.R
@@ -0,0 +1,9 @@
+library(readQP)
+
+context("read CPLEX QP files")
+
+test_that("read a CPLEX file successfully", {
+  test_path <- normalizePath("./test_data/simple_qp.lp")
+  file <- readQP(test_path)
+  expect_equal(file, "Minimize\nobj: a + b + [ a^2 + 4 a * b + 7 b^2 ]/2\nSubject To\nc1: a + b >= 10\nEnd\n")
+})
