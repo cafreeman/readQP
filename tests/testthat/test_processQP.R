7a0c79b8b0ca72ee505da8fefdad94e017566ed8
Chris Freeman <cfreeman.code@gmail.com>
2016-03-02 16:36:29 -0600

    Add tests for `processQP`

---
+++ tests/testthat/test_processQP.R
@@ -0,0 +1,21 @@
+context("Read a model file and return the parsed Q components")
+
+expected <- data.frame(
+  x1 = c("a", "b", "a", "b"),
+  x2 = c("a", "a", "b", "b"),
+  x3 = c("a^2", "b*a", "a*b", "b^2"),
+  values = c("1", NA, "+4", "+7"),
+  stringsAsFactors = FALSE
+  )
+
+test_that("processQP can read model a file and return the Q components", {
+  test_path <- normalizePath("./test_data/simple_qp.lp")
+  result <- processQP(test_path)
+  expect_is(result, "data.frame")
+  expect_equal(length(result), 4)
+  expect_equal(names(result), c("x1", "x2", "x3", "values"))
+  for (i in names(result)) {
+    expect_equal(result$i, expected$i)
+  }
+  expect_false(all(is.na(result)))
+})
