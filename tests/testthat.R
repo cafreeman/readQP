c9da32a1d8ea42355e735ea32500dda7676dfaa4
Chris Freeman <cfreeman.code@gmail.com>
2016-03-03 10:04:13 -0600

    Tests for `createQMatrix`

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
 library(testthat)
+library(slam)
 library(readQP)
 
 test_check("readQP")
---
+++ tests/testthat/test_createQMatrix.R
@@ -0,0 +1,44 @@
+context("Create Q matrix a coefficient mapping")
+
+test_that("createQMatrix works with a basic example", {
+  expected <- as.simple_triplet_matrix(matrix(c(1, 2, 2, 7), nrow = 2, ncol = 2))
+  path <- getSampleData("simple_qp", TRUE)
+  qVec <- processQP(path)
+  testQ <- createQMatrix(qVec)
+  expect_equal(testQ, expected)
+})
+
+test_that("createQMatrix works with >2 terms", {
+  expected <- as.simple_triplet_matrix(
+    matrix(
+      c(1, 2, 2.5, 2, 7, -1, 2.5, -1, 3),
+      nrow = 3,
+      ncol = 3
+      )
+    )
+  path <- getSampleData("qp_3_terms.lp", TRUE)
+  qVec <- processQP(path)
+  testQ <- createQMatrix(qVec)
+  expect_equal(testQ, expected)
+})
+
+test_that("createQMatrix works with missing terms", {
+  expected <- as.simple_triplet_matrix(
+    matrix(
+      c(1, 0, 2.5, 0, 7, -1, 2.5, -1, 3),
+      nrow = 3,
+      ncol = 3
+      )
+    )
+  path <- getSampleData("qp_missing_term", TRUE)
+  qVec <- processQP(path)
+  testQ <- createQMatrix(qVec)
+  expect_equal(testQ, expected)
+})
+
+test_that("createQMatrix errors when receiving incorrect input", {
+  path <- getSampleData("qp_3_terms", TRUE)
+  qVec <- processQP(path)
+  expect_error(createQMatrix(subset(qVec, !is.na(qVec$values))),
+               "The vector you provided is not the correct ")
+})
