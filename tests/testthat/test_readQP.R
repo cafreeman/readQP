476f2f10ef504900fe608ee56e95c9547773c72c
Chris Freeman <cfreeman.code@gmail.com>
2016-03-04 14:41:03 -0600

    More test cases for `readQP`

--- tests/testthat/test_readQP.R
+++ tests/testthat/test_readQP.R
@@ -1,12 +1,12 @@
 context("read CPLEX QP files")
 
-test_that("read a CPLEX file successfully", {
+test_that("readModelFile can read a CPLEX file successfully", {
   test_path <- getSampleData("simple_qp", TRUE)
   file <- readModelFile(test_path)
   expect_equal(file, "Minimize\nobj: a + b + [ a^2 + 4 a * b + 7 b^2 ]/2\nSubject To\nc1: a + b >= 10\nEnd\n")
 })
 
-test_that("error when the user doesn't point to a CPLEX file", {
+test_that("readModelFile errors when the user doesn't point to a CPLEX file", {
   test_path <- getSampleData("non_CPLEX.txt", TRUE)
   expect_error(readModelFile(test_path), "You must use a CPLEX LP model file \\(ending in '.lp'\\)")
 })
@@ -16,4 +16,26 @@ test_that("readQP correctly short-circuits when it encounters a model without a 
   expect_message(readQP(test_path), "^No quadratic elements detected\\. .+$")
   result <- suppressMessages(readQP(test_path))
   expect_is(result, c("MP_data_from_file", "MILP"))
+})
+
+test_that("readQP can correctly turn a QP model file into a model object", {
+  test_path <- getSampleData("qp_3_terms", TRUE)
+  model <- readQP(test_path)
+  expect_is(model, c("MP_data_from_file", "MILP"))
+  expect_equal(names(model$objective), c("c", "Q"))
+  list[c, Q] <- model$objective
+  expect_equal(c$v, c(1, 1, 2))
+  expect_is(c, "simple_triplet_matrix")
+  expect_equal(Q$v, c(1.0, 2.0, 2.5, 2.0, 7.0, -1.0, 2.5, -1.0, 3.0))
+  expect_is(Q, "simple_triplet_matrix")
+})
+
+test_that("readQP can handle in an insane LP file and return the correct object", {
+  test_path <- getSampleData("alldiet", TRUE)
+  expect_message(model <- readQP(test_path), "No quadratic elements detected. Defaulting to Rglpk_read_file.")
+  expect_output(model, "A linear program with 15 objective variables")
+  expect_equal(attr(model, "n_objective_vars"), 15)
+  expect_equal(attr(model, "n_objective_vars"), 15)
+  expect_equal(attr(model, "n_constraints"), 8)
+  expect_equal(attr(model, "objective_name"), "DOLLARS")
 })
